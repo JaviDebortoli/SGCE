0fc3878d59e22da29c5762d815b6149fa63abd53
JaviDebortoli <javidebortoli@gmail.com>
2026-02-08 12:31:40 -0300

    añadida vista de todas las inscripciones

--- src/main/java/SGCE/controller/EnrollmentController.java
+++ src/main/java/SGCE/controller/EnrollmentController.java
@@ -1,6 +1,10 @@
 package SGCE.controller;
 
+import SGCE.dto.CourseDto;
+import SGCE.dto.EnrollmentDto;
+import SGCE.service.CourseService;
 import SGCE.service.EnrollmentService;
+import SGCE.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +18,12 @@ public class EnrollmentController {
 
     @GetMapping()
     public String listEnrollments(Model model) {
-        enrollmentService.getAllEnrollments();
+        model.addAttribute("enrollments",enrollmentService.getAllEnrollments());
+        return "enrollments/enrollments-list";
+    }
+
+    @GetMapping("/new")
+    public String newEnrollment() {
         return "enrollments/enrollments";
     }
 
